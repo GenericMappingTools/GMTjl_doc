fad60129c14a6d0e62dd207ca477f7812e6eaec3
joa-quim <jluis@ualg.pt>
2022-07-09 02:15:52 +0000

    Deploying to gh-pages from  @ bf36657324ba39ae0788cb5be3aa9a5719fe2ce3 🚀

--- assets/documentation/modules/basemap/code/example_figure.jl
+++ assets/documentation/modules/basemap/code/example_figure.jl
@@ -4,9 +4,9 @@ begin # hide
 	GMT.isFranklin[1] = true    # hide
 	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
 	using GMT
-    basemap(region=(0,360,0,1000), figsize=15, frame=:afg, proj=:polar, show=true)
+    basemap(region=(0,360,0,1000), figsize=10, frame=:afg, proj=:polar, show=true)
 end # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_10239877286739101193.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_8235165079090378507.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
