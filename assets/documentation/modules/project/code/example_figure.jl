bee65cbaad55d9970cc381af7e75034dcb58c7ba
joa-quim <jluis@ualg.pt>
2022-07-13 13:49:52 +0000

    Deploying to gh-pages from  @ f581c645daf102de27fbd0e401f1bc230e6c70aa 🚀

--- assets/documentation/modules/project/code/example_figure.jl
+++ assets/documentation/modules/project/code/example_figure.jl
@@ -4,10 +4,10 @@ begin # hide
 	GMT.isFranklin[1] = true    # hide
 	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
 	using GMT
-Dellip = project(origin=(-30,70), step=10, ellipse="1500/600/30+e")
+Dellip = project(origin=(-30,70), step=10, ellipse="1500/600/30+e", km=true)
 imshow(Dellip, coast=true)
 end # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_11185430790584984190.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_13886887373886161454.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
