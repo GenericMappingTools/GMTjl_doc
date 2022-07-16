9d02649f7879648b0051ce49228f3c3f2575cc53
joa-quim <jluis@ualg.pt>
2022-07-16 02:25:42 +0000

    Deploying to gh-pages from  @ f10360902dbc06f5c6c7de127adcd9f846a98efc 🚀

---
+++ assets/documentation/modules/grdlandmask/code/example_figure.jl
@@ -0,0 +1,13 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+	using GMT   # hide
+	GMT.isFranklin[1] = true    # hide
+	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+	using GMT
+G = grdlandmask(region=:global360, res=:low, inc=1, maskvalues=(0,1,2,3,4))
+end # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_9867636046910884205.png"), force=true);    # hide
+GMT.isFranklin[1] = false    # hide
+GMT.IamModern[1]  = false    # hide
+ 
+nothing # hide
