c541138ed771d889ec6ecdd15d391929e93892ce
joa-quim <jluis@ualg.pt>
2022-07-21 17:40:21 +0000

    Deploying to gh-pages from  @ 09634cb33265982ae3881f81928087652757e80e 🚀

--- assets/examples/subplot/01_subplots/code/example_figure.jl
+++ assets/examples/subplot/01_subplots/code/example_figure.jl
@@ -10,9 +10,9 @@ n = 200;
 x, y, color = randn(n)/2, randn(n), randn(n);
 histogram(x, limits=(-4,4,0,40), binmethod="sqrt", panel=(1,1));
 scatter(x,y, limits=(-4,4,-4,4), marker=:circ, ms="10p", zcolor=color, panel=(2,1));
-histogram(x, limits=(-4,4,0,40), horizontal=true, binmethod="sqrt", panel=(2,2), show=true);
+histogram(y, limits=(-4,4,0,40), horizontal=true, binmethod="sqrt", panel=(2,2), show=true);
 end # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_15980707268236115471.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_9912761044480967764.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
