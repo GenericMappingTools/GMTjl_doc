f1e97eb35c4ab74b6d7f3c2082a3eb4e6969c26e
joa-quim <jluis@ualg.pt>
2023-09-02 01:34:16 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@7381a5130ee1ba380bbfd1f82dd7a40e21fd8a5c 🚀

--- assets/common_opts/opt_mfc.md
+++ assets/common_opts/opt_mfc.md
@@ -1,4 +1,4 @@
-- **G** or **markerfacecolor** or **MarkerFaceColor** or **mc** or **fill**\
+- **G** or **markerfacecolor** or **MarkerFaceColor** or **markercolor** or **mc** or **fill**\
    Select color or pattern for filling of symbols [Default is no fill]. Note that plot will search for *fill*
    and *pen* settings in all the segment headers (when passing a GMTdaset or file of a multi-segment dataset)
    and let any values thus found over-ride the command line settings (but those must be provided in the terse GMT
---
+++ assets/tutorials/vector_shp/vector_shp/code/example_figure.jl
@@ -0,0 +1,24 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+	using GMT   # hide
+	GMT.isFranklin[1] = true    # hide
+	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+	using GMT   # Hide
+munic = gmtread(GMT.TESTSDIR * "COL_ADM2.shp.zip");		# Hide
+mun_antioquia = filter(munic, NAME_1=:Antioquia);		# Hide
+antioquia_points = centroid(mun_antioquia);				# Hide
+t = info(mun_antioquia, att="ID_2");					# Hide
+# Convert t to numeric. Needed for creating a color map and make the choropleth style plot.
+tn = parse.(Int,t);
+
+# Create a color map
+C = makecpt(range=(minimum(tn),maximum(tn)), C=:bamako);
+
+# Vizualize 
+viz(mun_antioquia, proj=:guess, levels=tn, cmap=C, lw=0, title="Another Map of Antioquia", text=(data=antioquia_points, font=5), colorbar=true)
+end # hide
+mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * ".png"), joinpath(@OUTPUT, "example_6760807706937932694.png"), force=true);    # hide
+GMT.isFranklin[1] = false    # hide
+GMT.IamModern[1]  = false    # hide
+ 
+nothing # hide
