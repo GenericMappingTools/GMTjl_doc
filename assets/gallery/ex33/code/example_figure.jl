a9027ca0cd7839e95e3cfdc6ab0dd925f4e4a21e
joa-quim <jluis@ualg.pt>
2023-10-12 17:30:44 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@f767fab54fdd34ca939b79450d1fb874578cbfad 🚀

--- assets/gallery/ex33/code/example_figure.jl
+++ assets/gallery/ex33/code/example_figure.jl
@@ -9,7 +9,7 @@ resetGMT() # hide
 makecpt(cmap=:rainbow, range=(-5000,-2000))
 G = grdcut("@earth_relief_01m", region=(-118,-107,-49,-42))
 grdimage(G, shade=(angle=15, norm="e0.75"), proj=:merc)
-resetGMT()        # Need to find a fix for this. It lets the API in a geog mode
+#resetGMT()        # Need to find a fix for this. It lets the API in a geog mode
 
 # Select two points along the ridge
 ridge_pts = [-111.6 -43.0; -113.3 -47.5];
@@ -19,22 +19,18 @@ plot!(ridge_pts, region=G, symbol=(symb=:circle, size=0.25), fill=:blue, pen=(2,
 
 # Generate cross-profiles 400 km long, spaced 10 km, samped every 2km
 # and stack these using the median, write stacked profile
-table = grdtrack(G, ridge_pts, equidistant="400k/2k/10k+v", stack="m+sstack.txt")
+table, stack = grdtrack(G, ridge_pts, equidistant="400k/2k/10k+v", stack="m+s")
 plot!(table, pen=0.5)
 
 # Show upper/lower values encountered as an envelope
-env1 = gmtconvert("stack.txt", outcols="0,5")
-env2 = gmtconvert("stack.txt", outcols="0,6", reverse=true, suppress=true)
-env = [env1.data; env2.data];   # Concat the two matrices
-plot!(env, region=(-200,200,-3500,-2000), frame=(axes=:WSne, annot=:auto, ticks=:auto),
-      xaxis=(grid=1000, label="Distance from ridge (km)"), ylabel="Depth (m)", proj=:linear,
-      figsize=(15,7.5), fill=:lightgray, yshift=16)
-plot!("stack.txt", pen=3)
+plot!(stack[:,[1,2,6,7]], region=(-200,200,-3500,-2000), frame=(axes=:WSne, annot=:auto, ticks=:auto),
+      xaxis=(grid=1000, label="Distance from ridge (km)"), ylabel="Depth (m)",
+      figsize=(15,7.5), fill=:lightgray, close=(envelope=true,), yshift=16)
+plot!(stack, pen=3)
 text!(mat2ds([0 -2000], "MEDIAN STACKED PROFILE"), fill=:white, font=14, justify=:TC,
       offset=(away=true, shift=0.25), show=true)
-rm("stack.txt")
 end # hide
-mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * "." * "png"), joinpath(@OUTPUT, "example_1408503417161316490.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * "." * "png"), joinpath(@OUTPUT, "example_9103115099362066303.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
--- assets/gallery/ex46/code/example_figure.jl
+++ assets/gallery/ex46/code/example_figure.jl
@@ -7,8 +7,7 @@ begin # hide
 resetGMT() # hide
 
 coast(limits=:global, frame=(annot=:a,ticks=:a,grid=:a), proj=:EckertVI,
-      area=5000, shore=0.5, borders=(type=1, pen=(0.5,:gray)), water=[175 210 255],
-      par=(:MAP_FRAME_TYPE, :plain), figsize=16)
+      area=5000, shore=0.5, borders=(type=1, pen=(0.5,:gray)), water=[175 210 255])
 solar!(terminators=(term=:d, date="2016-02-09T16:00:00"), fill="navy@95")
 solar!(terminators=(term=:c, date="2016-02-09T16:00:00"), fill="navy@85")
 solar!(terminators=(term=:n, date="2016-02-09T16:00:00"), fill="navy@80")
@@ -17,7 +16,7 @@ solar!(terminators=(term=:a, date="2016-02-09T16:00:00"), fill="navy@80")
 t = solar(sun=(date="2016-02-09T16:00:00",), format=true);
 plot!(t, symbol="k@sunglasses/1.5", fill=:yellow, show=true)
 end # hide
-mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * "." * "png"), joinpath(@OUTPUT, "example_4898841001151115675.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * "." * "png"), joinpath(@OUTPUT, "example_2037549677329362456.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
