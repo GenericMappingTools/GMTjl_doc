9ed840cccc139d3c1733d96d1172dfac62b1a327
joa-quim <jluis@ualg.pt>
2024-02-12 00:46:40 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@9f847b773c4fa7685ebef9973c6152585756017c 🚀

---
+++ assets/documentation/utilities/cubeplot/code/example_figure.jl
@@ -0,0 +1,17 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+	using GMT   # hide
+	GMT.isFranklin[1] = true    # hide
+	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+	using GMT
+
+cubeplot(GMT.TESTSDIR * "seila_gray.jpg",
+         GMT.TESTSDIR * "seis_section_rgb.jpg",
+         GMT.TESTSDIR * "seis_section_gray.jpg",
+         zsize=6, show=true)
+end # hide
+mv(joinpath(tempdir(), "GMTjl_" * GMT.tmpdir_usr[2] * "." * "png"), joinpath(@OUTPUT, "example_4199742326267480904.png"), force=true);    # hide
+GMT.isFranklin[1] = false    # hide
+GMT.IamModern[1]  = false    # hide
+ 
+nothing # hide
