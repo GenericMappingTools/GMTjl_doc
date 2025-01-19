79157bbb9787bb05adebc89d4ef449d6d21f8ee5
joa-quim <jluis@ualg.pt>
2025-01-19 02:25:19 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@6bc0db90242d170a0c6e9b3d7c6a07f4205f5a0e 🚀

---
+++ assets/documentation/utilities/imrankfilter/code/example_figure.jl
@@ -0,0 +1,17 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+I = gmtread(TESTSDIR * "assets/small_squares.png");
+J = imrankfilter(I, width=10);
+grdimage(I, figsize=6)
+grdimage!(J, figsize=6, xshift=6, show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_2880310623194447774.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
---
+++ assets/documentation/utilities/imsegment/code/example_figure.jl
@@ -0,0 +1,17 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+I = gmtread(TESTSDIR * "assets/bunny_cenora.jpg");
+J = imsegment(I, colors=7);
+grdimage(I, figsize=6)
+grdimage!(J, figsize=6, xshift=6, show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_5745539523485618305.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
