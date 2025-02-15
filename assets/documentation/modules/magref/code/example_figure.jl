980dd5b08a1bec9253a76106c31a428a3eccbd18
joa-quim <jluis@ualg.pt>
2025-02-15 01:41:59 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@93d70241770cbdf29d1307ebb139a46b1678264c 🚀

---
+++ assets/documentation/modules/magref/code/example_figure.jl
@@ -0,0 +1,15 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+G = magref(R=:d, onetime=2025);
+viz(G, coast=true, colorbar=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_667666928552161076.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
--- assets/gallery/ex40/code/example_figure.jl
+++ assets/gallery/ex40/code/example_figure.jl
@@ -15,9 +15,9 @@ plot!("@GSHHS_h_Australia.txt", pen=:faint, fill=(240,255,240))
 plot!("@GSHHS_h_Australia.txt", ms=0.025, fill=:red)
 T500k = gmtsimplify("@GSHHS_h_Australia.txt", tolerance="500k");
 t = gmtspatial("@GSHHS_h_Australia.txt", colinfo=:g, length=:k);
-area = GMT.@sprintf("Full area = %.0f km@+2@+", t.data[3]);
+area = @sprintf("Full area = %.0f km@+2@+", t.data[3]);
 t = gmtspatial(T500k, colinfo=:g, length=:k); 
-area_T500k = GMT.@sprintf("Reduced area = %.0f km@+2@+", t.data[3]);
+area_T500k = @sprintf("Reduced area = %.0f km@+2@+", t.data[3]);
 plot!(T500k, pen=(1,:blue))
 plot!(centroid, marker=:cross, ms=0.75, ml=3)
 text!(text_record([112 -10], "T = 500 km"), offset=(away=true, shift=(0.25,0.25)), font=18, justify=:TL)
@@ -28,14 +28,14 @@ plot!("@GSHHS_h_Australia.txt", pen=:faint, fill=(240,255,240))
 plot!("@GSHHS_h_Australia.txt", ms=0.025, fill=:red)
 T100k = gmtsimplify("@GSHHS_h_Australia.txt", tolerance="100k");
 t = gmtspatial(T100k, colinfo=:g, length=:k);
-area_T100k = GMT.@sprintf("Reduced area = %.0f km@+2@+", t.data[3]);
+area_T100k = @sprintf("Reduced area = %.0f km@+2@+", t.data[3]);
 plot!(T100k, pen=(1,:blue))
 plot!(centroid, marker=:cross, ms=0.75, ml=3)
 text!(text_record([112 -10], "T = 100 km"), offset=(away=true, shift=(0.25,0.25)), font=18, justify=:TL)
 text!(text_record(area), font=14, region_justify=:CM)
 text!(text_record(area_T100k), font=14, region_justify=:LB, offset=(away=true, shift=0.5), show=true)
 	end # hide
-	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_16165801691724161067.png"), force=true);    # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_17323143256885855065.png"), force=true);    # hide
 	GMT.isFranklin[1] = false    # hide
 	GMT.IamModern[1]  = false    # hide
  
