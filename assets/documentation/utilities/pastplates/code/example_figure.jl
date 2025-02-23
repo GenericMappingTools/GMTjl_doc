3b280919c8461c6813e9169e0b753172ab3677f9
joa-quim <jluis@ualg.pt>
2025-02-23 20:22:41 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@e40711384aa059ab2db7d92a8ba1ef4a5f9cff48 🚀

---
+++ assets/documentation/utilities/pastplates/code/example_figure.jl
@@ -0,0 +1,14 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+pastplates()
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_16069740528281012049.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
