ffa6f7b5231480bd6fb7063d503beb6bc9068711
joa-quim <jluis@ualg.pt>
2025-02-24 18:31:33 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@6ea831b39d88be92b4e43a55554eafa1c752c6da 🚀

---
+++ assets/documentation/utilities/findpeaks/code/example_figure.jl
@@ -0,0 +1,18 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+D = gmtread(TESTSDIR * "assets/example_spectrum.txt");
+peaks = findpeaks(D, min_dist=0.2)
+
+plot(D, title="Peaks at least 0.2 units apart")
+scatter!(D[peaks,:], mc=:red, show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_6306417019194858389.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
---
+++ assets/documentation/utilities/hampel/code/example_figure.jl
@@ -0,0 +1,22 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+t = (1:50) / 10;
+x = [1:2:40; 5t + (@. 6cos(t + 0.5(t)^2)); fill(40,20)];
+x[12] = -10; x[50:52] .= -12; x[79:82] .= [-5, 50, 55, 0];
+m = hampel(x, 4, threshold=0);
+y = hampel(x, 4);
+
+plot(x, marker=:point, mc=:blue, lc=:blue, label="Original", xlabel="k", ylabel="x_k")
+scatter!(m, ms="2p", mc=:red, MarkerEdgeColor=true, label="Median filter")
+scatter!(y, ms="2p", mc=:green, MarkerEdgeColor=true, label="Hampel filter", show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_3649320973332272706.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
