5af31807b63d51d8640e8fa80c1728c6d9b130f9
joa-quim <jluis@ualg.pt>
2025-03-31 00:06:19 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@ee27f3ccb94c9197f5d6f449249b9f789d1ac0d3 🚀

---
+++ assets/documentation/modules/piechart/code/example_figure.jl
@@ -0,0 +1,13 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+piechart([1,2,3,4], explode=2, labels=("A","B","C","D"), labelstyle="namepercent", show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_1886554787567572658.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
