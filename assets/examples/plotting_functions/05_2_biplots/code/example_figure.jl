bd0cd086757f489674f89dc35859b9f48d9e74a0
joa-quim <jluis@ualg.pt>
2025-07-12 22:28:03 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@a68661ba869baf88b1664c159d04e842a3aee0e7 🚀

---
+++ assets/examples/plotting_functions/05_2_biplots/code/example_figure.jl
@@ -0,0 +1,14 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+biplot(TESTSDIR * "iris.dat", figsize=6, obsnumbers=true, show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_13530070533359481784.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
