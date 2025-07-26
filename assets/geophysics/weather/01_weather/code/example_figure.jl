b1af94212ece8f6b20b4e2620ec965ba3e0be78a
joa-quim <jluis@ualg.pt>
2025-07-26 17:10:03 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@3cb0c973b07a44f82c73ba879b504df278b4597f 🚀

---
+++ assets/geophysics/weather/01_weather/code/example_figure.jl
@@ -0,0 +1,22 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+GMT.resetGMT() # hide
+
+# Download the ecmwf forcasts for the _u_ and _v_ components of the wind at 10 meters above the surface
+ecmwf(:forecast, var="10u", region="IHO23", filename="u10.grd")
+ecmwf(:forecast, var="10v", region="IHO23", filename="v10.grd")
+
+windbarbs("10u.grd", "10v.grd", Q="0.25c+s20+w0.15c", I="x10", proj=:guess)
+rm("u10.grd") # Hide
+rm("v10.grd") # Hide
+coast!(shore=true, show=true)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_14338868640594262037.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
