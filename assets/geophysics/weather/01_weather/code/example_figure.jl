31ee262e2031374f907152124fe34db3bfb0faad
joa-quim <jluis@ualg.pt>
2025-07-26 23:26:42 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@f6e487f07bb918ea4f337fc8584a7a35418700e1 🚀

--- assets/geophysics/weather/01_weather/code/example_figure.jl
+++ assets/geophysics/weather/01_weather/code/example_figure.jl
@@ -10,12 +10,12 @@ GMT.resetGMT() # hide
 ecmwf(:forecast, var="10u", region="IHO23", filename="u10.grd")
 ecmwf(:forecast, var="10v", region="IHO23", filename="v10.grd")
 
-windbarbs("10u.grd", "10v.grd", Q="0.25c+s20+w0.15c", I="x10", proj=:guess)
+windbarbs("u10.grd", "v10.grd", Q="0.25c+s20+w0.15c", I="x10", proj=:guess)
 rm("u10.grd") # Hide
 rm("v10.grd") # Hide
 coast!(shore=true, show=true)
 	end # hide
-	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_14338868640594262037.png"), force=true);    # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_16445691025554754079.png"), force=true);    # hide
 	GMT.isFranklin[1] = false    # hide
 	GMT.IamModern[1]  = false    # hide
  
