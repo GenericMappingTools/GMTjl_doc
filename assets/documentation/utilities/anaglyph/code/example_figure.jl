8bb0a503fec144bc45737740d984c27d1d5e7121
joa-quim <jluis@ualg.pt>
2025-09-09 17:40:10 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@bbcda18c473392a98bec9e95b4cb3187ba89096d 🚀

---
+++ assets/documentation/utilities/anaglyph/code/example_figure.jl
@@ -0,0 +1,15 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+		using GMT   # hide
+		GMT.isFranklin[1] = true    # hide
+		getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+		using GMT
+
+I = anaglyph("@earth_relief_30s", R="-175.8/-171/-23/-14", view3d=true)
+viz(I)
+	end # hide
+	mv(joinpath(tempdir(), "GMTjl_" * GMT.TMPDIR_USR[2] * "." * "png"), joinpath(@OUTPUT, "example_2092062161487837990.png"), force=true);    # hide
+	GMT.isFranklin[1] = false    # hide
+	GMT.IamModern[1]  = false    # hide
+ 
+	nothing # hide
