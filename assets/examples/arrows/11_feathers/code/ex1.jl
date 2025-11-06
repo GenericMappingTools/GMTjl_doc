cee3e9b3b0d56baf604a10fb4c771b37b891a041
joa-quim <jluis@ualg.pt>
2025-11-06 23:38:07 +0000

    Deploying to gh-pages from @ GenericMappingTools/GMTjl_doc@cc0e64e2f34dca752a831d8087b95cb07fad0837 🚀

--- assets/examples/arrows/11_feathers/code/ex1.jl
+++ assets/examples/arrows/11_feathers/code/ex1.jl
@@ -3,5 +3,5 @@ using GMT, PrettyTables   # hide
 getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
 io = IOBuffer() # hide
 D = gmtread(TESTSDIR * "assets/wind_faro_s.dat")
-pretty_table(io, D.data; header=D.colnames, backend=Val(:html))	# hide
+PrettyTables.pretty_table(io, D.data; header=D.colnames, backend=Val(:html))	# hide
 println("~~~" * String(take!(io)) * "~~~") # hide
